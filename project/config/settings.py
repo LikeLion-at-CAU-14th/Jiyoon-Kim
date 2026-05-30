8f849f6141bbb2aec79aafc97152681083f8c764
yoon3660 <yoon3660@cau.ac.kr>
2026-05-30 23:04:23 +0900

    FEAT: Google 로그인

--- project/config/settings.py
+++ project/config/settings.py
@@ -50,6 +50,11 @@ THIRD_PARTY_APPS = [
  "corsheaders",
  "rest_framework",
  "rest_framework_simplejwt",
+ "allauth",
+ "allauth.account",
+ "allauth.socialaccount",
+ "allauth.socialaccount.providers.google",
+ # "allauth.socialaccount.providers.{제공_업체}" 찾아서 사용 가능
 ]
 
 
@@ -65,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -218,3 +224,7 @@ SIMPLE_JWT = { # JWT 세부내용 설정
     'BLACKLIST_AFTER_ROTATION': False,
     'TOKEN_USER_CLASS': 'accounts.User',
 }
+
+
+ACCOUNT_LOGIN_METHODS = {'email'}                  # 로그인 방식 설정
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*']    # 회원가입 시 필수 입력 필드 설정
